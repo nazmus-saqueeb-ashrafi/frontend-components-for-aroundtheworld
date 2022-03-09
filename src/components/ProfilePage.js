616604003860196d111a79f73f93c8ff7805ce74
Nazmus Ashrafi <nazmus.s.ashrafi@gmail.com>
2022-03-10 05:12:13 +0600

    made expanded post component (incomplete)

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -4,6 +4,8 @@ import { motion } from "framer-motion"
 import { useRef, useState } from "react";
 
 import { useDimensions } from "../useDimentions"
+import UnExpandedPostMaker from './profilePageComponents/UnExpandedPostMaker';
+import ExpandedPostMaker from './profilePageComponents/ExpandedPostMaker';
 
 
 const ProfilePage = () => {
@@ -110,9 +112,9 @@ const ProfilePage = () => {
             <div class=" absolute card w-100 bg-base-100 shadow-xl grid place-items-center" ref={constraintsRef}>
                 <Map
                     initialViewState={{
-                        longitude: -122.4,
-                        latitude: 37.8,
-                        zoom: 14
+                        longitude: 23.8103,
+                        latitude: 44.57875,
+                        zoom: 10
                     }}
                     style={{width: "90vw", height: 400}}
                     mapStyle="mapbox://styles/mapbox/streets-v9"
@@ -144,47 +146,16 @@ const ProfilePage = () => {
                 <a class="tab tab-bordered">Friends</a>
             </div>
 
-            {/* 'make a post' card */}
-            <div className='flex justify-start items-center bg-base-100 shadow-xl z-10 xl:col-start-1 xl:col-span-3 pt-2 pb-2 xl:pr-10 xl:pl-10 pr-5 pl-5 w-full rounded-xl'>
+            {/* 'unexpanded post' card */}
+             <UnExpandedPostMaker/>
 
-                {/* avatar */}
-                <div class="avatar pr-10">
-                    <div class="md:w-20 w-16 mask mask-squircle">
-                        <img src="https://api.lorem.space/image/face?hash=92048"/>
-                    </div>
-                </div>
+
+            {/* 'expanded post' card */}
+            <ExpandedPostMaker/>
+
+
+
                 
-                <div class='flex-grow pt-4'>
-                    <div class=" btn-outline cursor-pointer rounded-full p-2 bg-slate-600 hover:bg-slate-500 hover:text-slate-50"><span class='text-lg pl-3'>Where did you go, Simon?</span></div>
-
-
-                    <div class='flex justify-center items-center xl:p-3 p-1 space-x-3'>
-                        <button class="btn btn-ghost hover:bg-slate-600 flex-grow rounded-full normal-case font-normal">Location</button>
-
-                        <button class="btn btn-ghost hover:bg-slate-600 flex-grow rounded-full normal-case font-normal ">Photo</button>
-                        
-                        <button class="flex btn btn-ghost hover:bg-slate-600 flex-grow rounded-full normal-case font-normal">
-                            <svg class="w-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                                <path fill-rule="evenodd" d="M6.707 4.879A3 3 0 018.828 4H15a3 3 0 013 3v6a3 3 0 01-3 3H8.828a3 3 0 01-2.12-.879l-4.415-4.414a1 1 0 010-1.414l4.414-4.414zm4 2.414a1 1 0 00-1.414 1.414L10.586 10l-1.293 1.293a1 1 0 101.414 1.414L12 11.414l1.293 1.293a1 1 0 001.414-1.414L13.414 10l1.293-1.293a1 1 0 00-1.414-1.414L12 8.586l-1.293-1.293z" clip-rule="evenodd" />
-                            </svg>
-                            <span>Mood</span>
-                        </button>
-
-                        
-
-                    </div>
-                    
-
-                </div>
-                
-
-                {/* md:w-96 w-44 */}
-                
-            </div>
-
-
-        
-
         </div>
 
         
